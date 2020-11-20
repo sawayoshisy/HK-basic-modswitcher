b3536b9edd58169dfdc4788236f06c283ba54ace
ManicJamie <30422476+ManicJamie@users.noreply.github.com>
2020-11-20 00:17:14 +0000

    add files kekw (20/11/2020 v.57)

---
+++ modded.bat
@@ -0,0 +1,2 @@
+del hollow_knight_Data\Managed\Assembly-CSharp.dll
+copy /y Assembly-CSharp-Modded.dll hollow_knight_Data\Managed\Assembly-CSharp.dll
---
+++ vanilla.bat
@@ -0,0 +1,2 @@
+del hollow_knight_Data\Managed\Assembly-CSharp.dll
+copy /y Assembly-CSharp-Vanilla.dll hollow_knight_Data\Managed\Assembly-CSharp.dll
